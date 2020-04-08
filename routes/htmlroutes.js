40e226f3f1e4050fb4f046d29233ed9f4c39571b
Jessica Pavone <JessicaPavone24@yahoo.com>
2020-04-08 15:57:53 -0700

    fixing

--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -5,5 +5,5 @@ var path = require("path")
 router.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/index.html"));
 });
-
+    
 module.exports = router;
--- server.js
+++ server.js
@@ -3,7 +3,8 @@
 var express = require("express");
 var cors = require('cors')
 var app = express();
-var PORT =  process.env.port || 3000;
+var port = process.env.PORT || 8080;
+var htmlroutes = require('./routes/htmlroutes');
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -11,11 +12,9 @@ app.use(express.json());
 app.use(express.static(__dirname + '/public'));
 app.use(cors())
 
-var htmlroutes = require('./routes/htmlroutes');
 app.use('/', htmlroutes);
 
-
-app.listen(PORT, function() {
-  console.log("App listening on PORT " + PORT);
+app.listen(port, function() {
+  console.log("App listening on PORT " + port);
 });
   
