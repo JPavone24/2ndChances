40dad7d670652dd41fafe9889c4de419e78e1310
Jessica Pavone <JessicaPavone24@yahoo.com>
2020-04-08 17:10:21 -0700

    adding 4 more pages

---
+++ public/js/navbar.js
@@ -0,0 +1,28 @@
+
+$('#navbar').append(
+  '<nav class="navbar navbar-expand-lg navbar-dark  static-top" style="background-color: rgba(129, 87, 68, 0.76)" id="mainNav">' +
+    '<div class="container">' +
+      '<a class="navbar-brand js-scroll-trigger" href="/">2nd Chances</a>' +
+      '<button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">Menu<i class="fas fa-bars"></i></button>'+
+  
+      '<div class="collapse navbar-collapse" id="navbarResponsive">'+
+        '<ul class="navbar-nav text-uppercase ml-auto">' +
+          '<li class="nav-item">'+
+            '<a class="nav-link js-scroll-trigger" href="/">Home</a>'+
+          '</li>'+
+          '<li class="nav-item">'+
+            '<a class="nav-link js-scroll-trigger" href="/signup">Sign Up</a>'+
+          '</li>'+
+          '<li class="nav-item">'+
+              '<a class="nav-link js-scroll-trigger" href="/login">Login</a>'+
+          '</li>'+
+          '<li class="nav-item">'+
+            '<a class="nav-link js-scroll-trigger" href="/gallery">Pet Gallery</a>'+
+          '<li class="nav-item">'+
+              '<a class="nav-link js-scroll-trigger" href="/register">Register Pet</a>'+
+          '</li>'+
+        '</ul>'+
+      '</div>'+
+    '</div>'+
+  '</nav>'
+  )
--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -5,5 +5,22 @@ var path = require("path")
 router.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/index.html"));
 });
-    
+
+
+router.get("/signup", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/signup.html"));
+});
+
+router.get("/login", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/login.html"));
+});
+
+router.get("/gallery", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/gallery.html"));
+ });
+
+router.get("/register", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/register.html"));
+});
+
 module.exports = router;
