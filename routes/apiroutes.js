e4401ea3a2d141c97a89978451678391521c998f
Jessica Pavone <JessicaPavone24@yahoo.com>
2020-04-08 18:33:54 -0700

    adding last bit

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -3,7 +3,38 @@ var router = express.Router();
 const mongoose = require('mongoose')
 let PetsData = require('../models/pets.model');
 var ObjectId = require('mongodb').ObjectID;
+const multer = require("multer")
 
+const storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    cb(null, './public/uploads/');
+  },
+  filename: function(req, file, cb) {
+    cb(null, file.originalname);
+  }
+});
+
+const upload = multer({
+  storage: storage,
+});
+
+router.post('/pets', upload.single('img'),function (req, res) {
+    console.log(req.body)
+    console.log(req.file)
+  const Pet = new PetsData({
+    _id: new mongoose.Types.ObjectId(),
+    typePet : req.body.typePet,
+    typeBreed : req.body.typeBreed,
+    petName : req.body.petName.toLowerCase(),
+    bio :  req.body.bio,
+    img: req.file.filename
+})  
+ Pet.save()  
+.then(result =>{
+    console.log(result)
+})
+res.end()
+})
 
 router.get("/pets", function(req, res) {
   PetsData.find({})
